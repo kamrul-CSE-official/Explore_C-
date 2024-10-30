332ff3a89fc0d7f63c53b13d04a3e76e8e941c64
MD.Kamrul Hasan ⚡️ <kamrulhasan635653@gmail.com>
2024-10-31 00:31:53 +0600

    Update

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿string name = "MD.Kamrul Hasan";
+int age = 24;
+age = 25;
+double weight = 65.40;
+const double height = 5.9;
+char nameFirstLater = 'K';
+bool isMuslim = true;
+
+//Console.WriteLine(string.Format("Name: {0}, \nAge: {1}, \nWeight: {2}, \nFirst letter in name: {3}, \nIs Muslim: {4}, \nHeight: {5}",
+//name, age, weight, nameFirstLater, isMuslim, height));
+
+//Console.WriteLine($"Name: {name}, \nAge: {age}, \nWeight: {weight}, \nFirst letter in name: {nameFirstLater}, \nIs Muslim: {isMuslim}, \nHeight: {height}");
+
+
+
+int x = 5, y = 6, z = 50;
+Console.WriteLine(x + y + z);
+
+
+//https://www.w3schools.com/cs/cs_type_casting.php
